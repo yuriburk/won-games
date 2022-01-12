261ea05fea7c985457ca848c147db75b5624d233
Yuri Burk <yuriburk@gmail.com>
2022-01-12 17:27:53 -0300

    Logo tests

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,9 +1,21 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Logo from 'components/Logo'
+import { renderWithTheme } from 'utils/tests/helpers'
+import theme from 'styles/theme'
 
 describe('<Logo />', () => {
   it('should render the white logo by default', () => {
-    const { container } = render(<Logo />)
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: theme.colors.white
+    })
+  })
+
+  it('should render the black label when the color is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: theme.colors.black
+    })
   })
 })
