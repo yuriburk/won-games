e3854c7acd402d18c4bc366703fdf5c49aa1e487
Yuri Burk <yuriburk@gmail.com>
2022-01-12 17:23:16 -0300

    Render with theme provider util

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,11 +3,7 @@ import { render, screen } from '@testing-library/react'
 import Logo from 'components/Logo'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
+  it('should render the white logo by default', () => {
     const { container } = render(<Logo />)
-
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
   })
 })
