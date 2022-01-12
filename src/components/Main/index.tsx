a671c3578240879d4cc81854aa38e16112d7191a
Yuri Burk <yuriburk@gmail.com>
2022-01-12 17:02:05 -0300

    Storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,18 +1,21 @@
 import * as S from 'components/Main/styles';
 
+import logo from '../../../public/img/logo.svg';
+import illustration from '../../../public/img/hero-illustration.svg';
+
 const Main = ({
   title = 'React Avançado',
   description = 'TypeScript, ReactJS, NextJS e Styled Components'
 }) => (
   <S.Wrapper>
     <S.Logo
-      src="/img/logo.svg"
+      src={logo}
       alt="Imagem de um átomo e React Avançado escrito ao lado."
     />
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
     <S.Illustration
-      src="/img/hero-illustration.svg"
+      src={illustration}
       alt="Um desenvolvedor de frente para uma tela com código."
     />
   </S.Wrapper>
