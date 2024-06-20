e9c9a1a72b289432a63c684326b49ae3f128def0
Yuri Burk <yuriburk@gmail.com>
2024-06-19 22:56:44 -0300

    Updating to new next version

--- src/components/MediaMatch/types.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultBreakpoints } from 'styled-media-query'
-
-export type Breakpoint = keyof DefaultBreakpoints
-
-export type MediaMatchProps = {
-  lessThan?: Breakpoint
-  greaterThan?: Breakpoint
-}
