e67ba1848a0741a3232cb9279b8bf8688ff54dcd
Yuri Burk <yuriburk@gmail.com>
2024-06-22 18:24:32 -0300

    Create husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,5 +9,6 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReactConfig
+  pluginReactConfig,
+  { env: { browser: true, amd: true, node: true } }
 ]
